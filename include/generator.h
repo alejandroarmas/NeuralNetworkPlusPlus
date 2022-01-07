3acbfec9439d316dc43731abbfcc4386e5273a9d
= <dlmhsarmasalejandro@gmail.com>
2022-01-06 16:36:27 -0800

    Generator classes: Normal Distribution and Tester with constant vals.

--- include/generator.h
+++ include/generator.h
@@ -8,31 +8,59 @@
 
 namespace Matrix {
 
-    template <class T>
-    class BaseGenerator {
+    namespace Generation {
 
-        public:
-            virtual std::unique_ptr<Matrix::Representation<T>> operator() (std::unique_ptr<Matrix::Representation<T>> m) = 0;
-    };
+        template <class T>
+        class Base {
+
+            public:
+                virtual std::unique_ptr<Matrix::Representation<T>> operator() (std::unique_ptr<Matrix::Representation<T>> m) = 0;
+        };
 
 
-    template <class T>
-    class Generator : public BaseGenerator<T> {
+        template <class T>
+        class Normal : public Base<T> {
 
-        public:
-            std::unique_ptr<Matrix::Representation<T>> operator() (std::unique_ptr<Matrix::Representation<T>> m) override;
-    };
+            public:
+                Normal(double _m, double _v) : mean(_m), variance(_v) {}
+                std::unique_ptr<Matrix::Representation<T>> operator() (std::unique_ptr<Matrix::Representation<T>> m) override;
+            private:
+                double  mean; 
+                double variance;
+        };
 
-    template class Generator<float>;
-    template class Generator<double>;
-    template class Generator<int8_t>;
-    template class Generator<int16_t>;
-    template class Generator<int32_t>;
-    template class Generator<int64_t>;
-    template class Generator<uint8_t>;
-    template class Generator<uint16_t>;
-    template class Generator<uint32_t>;
-    template class Generator<uint64_t>;
+        template class Normal<float>;
+        template class Normal<double>;
+        template class Normal<int8_t>;
+        template class Normal<int16_t>;
+        template class Normal<int32_t>;
+        template class Normal<int64_t>;
+        template class Normal<uint8_t>;
+        template class Normal<uint16_t>;
+        template class Normal<uint32_t>;
+        template class Normal<uint64_t>;
+
+
+        template <class T>
+        class Tester : public Base<T> {
+
+            public:
+                std::unique_ptr<Matrix::Representation<T>> operator() (std::unique_ptr<Matrix::Representation<T>> m) override;
+            private:
+        };
+
+        template class Tester<float>;
+        template class Tester<double>;
+        template class Tester<int8_t>;
+        template class Tester<int16_t>;
+        template class Tester<int32_t>;
+        template class Tester<int64_t>;
+        template class Tester<uint8_t>;
+        template class Tester<uint16_t>;
+        template class Tester<uint32_t>;
+        template class Tester<uint64_t>;
+
+    }
 
 }
 
