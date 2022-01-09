d12d36db68ffebbcc952e249a8bd6eff2b5e25e8
Alejandro Armas <dlmhsarmasalejandro@gmail.com>
2022-01-08 20:05:04 -0800

    Added float comparison operator.

--- functions.cpp
+++ functions.cpp
@@ -1,13 +1,17 @@
 
-#include <math.h>
+#include <math.h>   // fabs(), isinf(), isnan()
+#include <string.h> // memset: zeros out all the bytes in an array
+#include <stdint.h> // uint_64t, INT32_MAX
+#include <stdlib.h>
+#include <stdio.h>
 
 #include "functions.h"
-
+#include "config.h"
 
 
 
 template <class T>
-T ActivationFunctions::Sigmoid<T>::operate(T x) {
+T Functions::Activation::Sigmoid<T>::operate(T x) {
 
     T denominator = 1 + ::exp(x); 
     T output = 1 / denominator; 
@@ -16,9 +20,9 @@ T ActivationFunctions::Sigmoid<T>::operate(T x) {
 }
 
 template <class T>
-T ActivationFunctions::Differentiable::Sigmoid<T>::operate(T x) {
+T Functions::Activation::Differentiable::Sigmoid<T>::operate(T x) {
 
-    ActivationFunctions::Sigmoid<T> sigmoid;
+    Functions::Activation::Sigmoid<T> sigmoid;
 
     T result = sigmoid.operate(x);
  
@@ -26,3 +30,48 @@ T ActivationFunctions::Differentiable::Sigmoid<T>::operate(T x) {
 
     return output;
 }
+
+
+
+// --------------------------------------------------
+/* --------------------------------------------------
+Code from: https://bitbashing.io/comparing-floats.html
+*/
+volatile bool Functions::Utility::compare_float(float a, float b) {
+    
+    const float difference = fabs(a - b);
+    if (difference <= EPSILON) return true;
+
+    return Functions::Utility::ulpsDistance(a, b) <= ULPS_EPSILON;
+}
+
+/*
+Units of Least Precisixon: How many mantisa bits of difference we tolerate.
+*/
+int32_t Functions::Utility::ulpsDistance(const float a, const float b)
+{
+    // Save work if the floats are equal.
+    // Also handles +0 == -0
+    if (a == b) return 0;
+
+    
+    // Max distance for NaN
+    if (isnan(a) || isnan(b)) return INT32_MAX;
+
+    // If one's infinite and they're not equal, max distance.
+    if (isinf(a) || isinf(b)) return INT32_MAX;
+
+    int32_t ia, ib;
+    memcpy(&ia, &a, sizeof(float));
+    memcpy(&ib, &b, sizeof(float));
+
+    // Don't compare differently-signed floats.
+    if ((ia < 0) != (ib < 0)) return INT32_MAX;
+
+    // Return the absolute value of the distance in ULPs.
+    int32_t distance = ia - ib;
+    if (distance < 0) distance = -distance;
+    return distance;
+}
+
+// --------------------------------------------------
--- include/functions.h
+++ include/functions.h
@@ -3,48 +3,65 @@
 
 #include <cstdint>
 
-namespace ActivationFunctions {
+namespace Functions {
 
-    
-    template <class T>
-    class BaseFunction {
+    namespace Activation {
 
-        public:
-            virtual T operate(T x) = 0;
-    };
+        template <class T>
+        class BaseFunction {
 
+            public:
+                virtual T operate(T x) = 0;
+        };
 
-    template<class T>
-    class Sigmoid : public BaseFunction<T> {
-
-        public:
-            T operate(T x) override;
-            
-    };
-
-
-    template class Sigmoid<float>;
-    template class Sigmoid<double>;
-    template class Sigmoid<int8_t>;
-    template class Sigmoid<int16_t>;
-    template class Sigmoid<int32_t>;
-    template class Sigmoid<int64_t>;
-    template class Sigmoid<uint8_t>;
-    template class Sigmoid<uint16_t>;
-    template class Sigmoid<uint32_t>;
-    template class Sigmoid<uint64_t>;
-
-    
-    namespace Differentiable {
 
         template<class T>
-        class Sigmoid : public ActivationFunctions::Sigmoid<T> {
+        class Sigmoid : public BaseFunction<T> {
 
             public:
                 T operate(T x) override;
                 
         };
+
+
+        template class Sigmoid<float>;
+        template class Sigmoid<double>;
+        template class Sigmoid<int8_t>;
+        template class Sigmoid<int16_t>;
+        template class Sigmoid<int32_t>;
+        template class Sigmoid<int64_t>;
+        template class Sigmoid<uint8_t>;
+        template class Sigmoid<uint16_t>;
+        template class Sigmoid<uint32_t>;
+        template class Sigmoid<uint64_t>;
+
+        
+        namespace Differentiable {
+
+            template<class T>
+            class Sigmoid : public Functions::Activation::Sigmoid<T> {
+
+                public:
+                    T operate(T x) override;
+                    
+            };
+        }
+
     }
+}
+
+
+
+namespace Functions {
+
+    namespace Utility {
+
+            /* Helper Functions for float comparison: */
+
+            volatile bool compare_float(float a, float b);
+            int32_t ulpsDistance(const float a, const float b);
+
+            }
 
 }
 
