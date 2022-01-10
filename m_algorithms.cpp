59a33ba933978109a2462ba44ec33fb6d774a783
Alejandro Armas <dlmhsarmasalejandro@gmail.com>
2022-01-09 17:31:11 -0800

    Organized implementations into namespaces.

--- m_algorithms.cpp
+++ m_algorithms.cpp
@@ -3,126 +3,156 @@
 #include <cilk/cilk.h>
 #include <cilk/reducer.h>
 
-std::unique_ptr<Matrix::Representation> Matrix::Operations::Add::Std::operator()(
-        Matrix::Representation l, 
-        Matrix::Representation r) {
 
-        std::transform(l.scanStart(), l.scanEnd(), r.scanStart(), l.scanStart(), std::plus<float>());
-        
-        auto output = std::make_unique<Matrix::Representation>(l);
+namespace Matrix {
 
-    return output;
-}
+    namespace Operations {
 
 
+        namespace Addition {
 
-std::unique_ptr<Matrix::Representation> Matrix::Operations::HadamardProduct::Std::operator()(
-        Matrix::Representation l, 
-        Matrix::Representation r) {
+            std::unique_ptr<Matrix::Representation> Std::operator()(
+                    Matrix::Representation l, 
+                    Matrix::Representation r) {
 
-        std::transform(l.scanStart(), l.scanEnd(), r.scanStart(), l.scanStart(), std::multiplies<float>());
-        
-        auto output = std::make_unique<Matrix::Representation>(l);
+                if ((l.num_rows() != r.num_rows()) && (l.num_cols() != r.num_cols())) {
+                    throw std::length_error("Matrix A not same size as Matrix B.");
+                }
+                    
+                auto output = std::make_unique<Matrix::Representation>(l.num_rows(), r.num_cols());
 
-    return output;
-}
+                std::transform(l.scanStart(), l.scanEnd(), r.scanStart(), output->scanStart(), std::plus<float>());
 
-
-
-
-std::unique_ptr<Matrix::Representation> Matrix::Operations::HadamardProduct::Naive::operator()(
-        Matrix::Representation l, 
-        Matrix::Representation r) {
-
-    if ((l.num_rows() != r.num_rows()) && (l.num_cols() != r.num_cols())) {
-        throw std::length_error("Matrix A columns not equal to Matrix B rows.");
-    }
-
-    std::unique_ptr<Matrix::Representation> output = std::make_unique<Matrix::Representation>(l.num_rows(), r.num_cols());
-
-
-    for (u_int64_t i = 0; i < l.num_rows(); i++) {
-        
-        for (u_int64_t j = 0; j < r.num_cols(); j++) {
-
-
-            float val = l.get(i, j) * r.get(i, j);
-
-            output->put(i, j, val);
-
+                return output;
+            }
         }
 
-    }
 
+        namespace HadamardProduct {
 
-    return output;
-}
+            std::unique_ptr<Matrix::Representation> Std::operator()(
+                    Matrix::Representation l, 
+                    Matrix::Representation r) {
 
+                    std::transform(l.scanStart(), l.scanEnd(), r.scanStart(), l.scanStart(), std::multiplies<float>());
+                    
+                    auto output = std::make_unique<Matrix::Representation>(l);
 
-
-std::unique_ptr<Matrix::Representation> Matrix::Operations::Multiplication::Naive::operator()(
-        Matrix::Representation l, 
-        Matrix::Representation r) {
-
-    if (l.num_cols() != r.num_rows()) {
-        throw std::length_error("Matrix A columns not equal to Matrix B rows.");
-    }
-
-    std::unique_ptr<Matrix::Representation> output = std::make_unique<Matrix::Representation>(l.num_rows(), r.num_cols());
-
-
-    for (u_int64_t i = 0; i < l.num_rows(); i++) {
-        
-        for (u_int64_t j = 0; j < r.num_cols(); j++) {
-
-
-            float val = 0;
-
-            for (u_int64_t k = 0; k < l.num_cols(); k++) {
-                val += l.get(i, k) * r.get(k, j);
+                return output;
             }
 
-            output->put(i, j, val);
 
-        }
+            std::unique_ptr<Matrix::Representation> Naive::operator()(
+                    Matrix::Representation l, 
+                    Matrix::Representation r) {
 
-    }
+                if ((l.num_rows() != r.num_rows()) && (l.num_cols() != r.num_cols())) {
+                    throw std::length_error("Matrix A not same size as Matrix B.");
+                }
 
-
-
-    return output;
-}
-
-
-std::unique_ptr<Matrix::Representation> Matrix::Operations::Multiplication::Square::operator()(
-        Matrix::Representation l, 
-        Matrix::Representation r) {
-
-    if (l.num_cols() != r.num_rows()) {
-        throw std::length_error("Matrix A columns not equal to Matrix B rows.");
-    }
-
-    std::unique_ptr<Matrix::Representation> output = std::make_unique<Matrix::Representation>(l.num_rows(), r.num_cols());
+                std::unique_ptr<Matrix::Representation> output = std::make_unique<Matrix::Representation>(l.num_rows(), r.num_cols());
 
 
-    cilk_for (u_int64_t i = 0; i < l.num_rows(); i++) {
-        
-        for (u_int64_t j = 0; j < r.num_cols(); j++) {
+                for (u_int64_t i = 0; i < l.num_rows(); i++) {
+                    
+                    for (u_int64_t j = 0; j < r.num_cols(); j++) {
 
 
-            float val = 0;
+                        float val = l.get(i, j) * r.get(i, j);
 
-            for (u_int64_t k = 0; k < l.num_cols(); k++) {
-                val += l.get(i, k) * r.get(k, j);
+                        output->put(i, j, val);
+
+                    }
+
+                }
+
+
+                return output;
+            }
+        } 
+
+
+        namespace Multiplication {
+
+            std::unique_ptr<Matrix::Representation> Naive::operator()(
+                    Matrix::Representation l, 
+                    Matrix::Representation r) {
+
+                if (l.num_cols() != r.num_rows()) {
+                    throw std::length_error("Matrix A columns not equal to Matrix B rows.");
+                }
+
+                std::unique_ptr<Matrix::Representation> output = std::make_unique<Matrix::Representation>(l.num_rows(), r.num_cols());
+
+
+                for (u_int64_t i = 0; i < l.num_rows(); i++) {
+                    
+                    for (u_int64_t j = 0; j < r.num_cols(); j++) {
+
+
+                        float val = 0;
+
+                        for (u_int64_t k = 0; k < l.num_cols(); k++) {
+                            val += l.get(i, k) * r.get(k, j);
+                        }
+
+                        output->put(i, j, val);
+
+                    }
+
+                }
+
+
+
+                return output;
             }
 
-            output->put(i, j, val);
 
+            std::unique_ptr<Matrix::Representation> Square::operator()(
+                    Matrix::Representation l, 
+                    Matrix::Representation r) {
+
+                if (l.num_cols() != r.num_rows()) {
+                    throw std::length_error("Matrix A columns not equal to Matrix B rows.");
+                }
+
+                std::unique_ptr<Matrix::Representation> output = std::make_unique<Matrix::Representation>(l.num_rows(), r.num_cols());
+
+
+                cilk_for (u_int64_t i = 0; i < l.num_rows(); i++) {
+                    
+                    for (u_int64_t j = 0; j < r.num_cols(); j++) {
+
+
+                        float val = 0;
+
+                        for (u_int64_t k = 0; k < l.num_cols(); k++) {
+                            val += l.get(i, k) * r.get(k, j);
+                        }
+
+                        output->put(i, j, val);
+
+                    }
+
+                }
+
+
+
+                return output;
+            }
         }
 
     }
 
-
-
-    return output;
 }
+
+
+
+
+
+
+
+
+
+
+
