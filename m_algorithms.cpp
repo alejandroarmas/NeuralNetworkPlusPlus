c032159e0900d770b916648c38d04a5cde25e67d
Alejandro Armas <dlmhsarmasalejandro@gmail.com>
2022-02-24 17:26:07 -0800

    Polymorphic objects need destructors even if purely virtual.

--- m_algorithms.cpp
+++ m_algorithms.cpp
@@ -12,16 +12,16 @@ namespace Matrix {
         namespace Addition {
 
             std::unique_ptr<Matrix::Representation> Std::operator()(
-                    Matrix::Representation l, 
-                    Matrix::Representation r) {
+                    std::unique_ptr<Matrix::Representation>& l, 
+                    std::unique_ptr<Matrix::Representation>& r) {
 
-                if ((l.num_rows() != r.num_rows()) && (l.num_cols() != r.num_cols())) {
+                if ((l->num_rows() != r->num_rows()) && (l->num_cols() != r->num_cols())) {
                     throw std::length_error("Matrix A not same size as Matrix B.");
                 }
                     
-                auto output = std::make_unique<Matrix::Representation>(l.num_rows(), r.num_cols());
+                auto output = std::make_unique<Matrix::Representation>(l->num_rows(), r->num_cols());
 
-                std::transform(l.scanStart(), l.scanEnd(), r.scanStart(), output->scanStart(), std::plus<float>());
+                std::transform(l->scanStart(), l->scanEnd(), r->scanStart(), output->scanStart(), std::plus<float>());
 
                 return output;
             }
@@ -31,34 +31,35 @@ namespace Matrix {
         namespace HadamardProduct {
 
             std::unique_ptr<Matrix::Representation> Std::operator()(
-                    Matrix::Representation l, 
-                    Matrix::Representation r) {
+                    std::unique_ptr<Matrix::Representation>& l, 
+                    std::unique_ptr<Matrix::Representation>& r) {
 
-                    std::transform(l.scanStart(), l.scanEnd(), r.scanStart(), l.scanStart(), std::multiplies<float>());
+                    auto output = std::make_unique<Matrix::Representation>(l->num_rows(), r->num_cols());
+
                     
-                    auto output = std::make_unique<Matrix::Representation>(l);
-
+                    std::transform(l->scanStart(), l->scanEnd(), r->scanStart(), output->scanStart(), std::multiplies<float>()); 
+                    
                 return output;
             }
 
 
             std::unique_ptr<Matrix::Representation> Naive::operator()(
-                    Matrix::Representation l, 
-                    Matrix::Representation r) {
+                    std::unique_ptr<Matrix::Representation>& l, 
+                    std::unique_ptr<Matrix::Representation>& r) {
 
-                if ((l.num_rows() != r.num_rows()) && (l.num_cols() != r.num_cols())) {
+                if ((l->num_rows() != r->num_rows()) && (l->num_cols() != r->num_cols())) {
                     throw std::length_error("Matrix A not same size as Matrix B.");
                 }
 
-                std::unique_ptr<Matrix::Representation> output = std::make_unique<Matrix::Representation>(l.num_rows(), r.num_cols());
+                std::unique_ptr<Matrix::Representation> output = std::make_unique<Matrix::Representation>(l->num_rows(), r->num_cols());
 
 
-                for (u_int64_t i = 0; i < l.num_rows(); i++) {
+                for (u_int64_t i = 0; i < l->num_rows(); i++) {
                     
-                    for (u_int64_t j = 0; j < r.num_cols(); j++) {
+                    for (u_int64_t j = 0; j < r->num_cols(); j++) {
 
 
-                        float val = l.get(i, j) * r.get(i, j);
+                        float val = l->get(i, j) * r->get(i, j);
 
                         output->put(i, j, val);
 
@@ -75,25 +76,25 @@ namespace Matrix {
         namespace Multiplication {
 
             std::unique_ptr<Matrix::Representation> Naive::operator()(
-                    Matrix::Representation l, 
-                    Matrix::Representation r) {
+                    std::unique_ptr<Matrix::Representation>& l, 
+                    std::unique_ptr<Matrix::Representation>& r) {
 
-                if (l.num_cols() != r.num_rows()) {
+                if (l->num_cols() != r->num_rows()) {
                     throw std::length_error("Matrix A columns not equal to Matrix B rows.");
                 }
 
-                std::unique_ptr<Matrix::Representation> output = std::make_unique<Matrix::Representation>(l.num_rows(), r.num_cols());
+                std::unique_ptr<Matrix::Representation> output = std::make_unique<Matrix::Representation>(l->num_rows(), r->num_cols());
 
 
-                for (u_int64_t i = 0; i < l.num_rows(); i++) {
+                for (u_int64_t i = 0; i < l->num_rows(); i++) {
                     
-                    for (u_int64_t j = 0; j < r.num_cols(); j++) {
+                    for (u_int64_t j = 0; j < r->num_cols(); j++) {
 
 
                         float val = 0;
 
-                        for (u_int64_t k = 0; k < l.num_cols(); k++) {
-                            val += l.get(i, k) * r.get(k, j);
+                        for (u_int64_t k = 0; k < l->num_cols(); k++) {
+                            val += l->get(i, k) * r->get(k, j);
                         }
 
                         output->put(i, j, val);
@@ -133,25 +134,25 @@ namespace Matrix {
             } }
             */
             std::unique_ptr<Matrix::Representation> Square::operator()(
-                    Matrix::Representation l, 
-                    Matrix::Representation r) {
+                    std::unique_ptr<Matrix::Representation>& l, 
+                    std::unique_ptr<Matrix::Representation>& r) {
 
-                if (l.num_cols() != r.num_rows()) {
+                if (l->num_cols() != r->num_rows()) {
                     throw std::length_error("Matrix A columns not equal to Matrix B rows.");
                 }
 
-                std::unique_ptr<Matrix::Representation> output = std::make_unique<Matrix::Representation>(l.num_rows(), r.num_cols());
+                std::unique_ptr<Matrix::Representation> output = std::make_unique<Matrix::Representation>(l->num_rows(), r->num_cols());
 
 
-                cilk_for (u_int64_t i = 0; i < l.num_rows(); i++) {
+                cilk_for (u_int64_t i = 0; i < l->num_rows(); i++) {
                     
-                    for (u_int64_t j = 0; j < r.num_cols(); j++) {
+                    for (u_int64_t j = 0; j < r->num_cols(); j++) {
 
 
                         float val = 0;
 
-                        for (u_int64_t k = 0; k < l.num_cols(); k++) {
-                            val += l.get(i, k) * r.get(k, j);
+                        for (u_int64_t k = 0; k < l->num_cols(); k++) {
+                            val += l->get(i, k) * r->get(k, j);
                         }
 
                         output->put(i, j, val);
