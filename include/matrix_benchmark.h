1ba14ba0aadf79a0f7041eae01c3155ccc9f8478
Alejandro Armas <dlmhsarmasalejandro@gmail.com>
2022-04-09 18:27:57 -0700

    Updated Documentation for Timer class.

--- include/matrix_benchmark.h
+++ include/matrix_benchmark.h
@@ -9,25 +9,6 @@
 namespace Matrix {
 
 
-    /* EXAMPLE USAGE:    
-    
-    using matrix_t = Matrix::Representation; 
-
-    std::unique_ptr<matrix_t> ma = std::make_unique<matrix_t>(4000, 2000);
-    std::unique_ptr<matrix_t> mb = std::make_unique<matrix_t>(2000, 3000);
-
-
-    Matrix::Generation::Normal<0, 1> normal_distribution_init;
-
-
-    ma = normal_distribution_init(std::move(ma));
-    mb = normal_distribution_init(std::move(mb));
-
-    std::unique_ptr<Matrix::Operations::Multiplication::Square> mul_ptr = std::make_unique<Matrix::Operations::Multiplication::Square>();
-    Matrix::Benchmark mul_bm(std::move(mul_ptr));
-    std::unique_ptr<matrix_t> mc = mul_bm(ma, mb);
-    
-    */
 
    class Benchmark {
 
@@ -42,6 +23,28 @@ namespace Matrix {
 
    };
 
+
+
+    /*
+    DESCRIPTION:
+
+        Decorator for BaseOp() class Function objects, used to benchmark algorithm performance.
+
+    USAGE:
+
+        using matrix_t = Matrix::Representation; 
+        std::unique_ptr<matrix_t> ma = std::make_unique<matrix_t>(5000, 5000);
+        std::unique_ptr<matrix_t> mb = std::make_unique<matrix_t>(5000, 5000);
+        Matrix::Generation::Normal<0, 1> normal_distribution_init;
+
+        ma = normal_distribution_init(std::move(ma));
+        mb = normal_distribution_init(std::move(mb));
+
+        std::unique_ptr<Matrix::Operations::Multiplication::ParallelDNC> mul_ptr_r       = std::make_unique<Matrix::Operations::Multiplication::ParallelDNC>();
+        Matrix::Timer mul_bm_r(std::move(mul_ptr_r));
+        std::unique_ptr<matrix_t> mf = mul_bm_r(ma, mb);
+
+    */
     class Timer : public Benchmark {
 
         public:
